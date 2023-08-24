6d16a9977cf3fa2dea2003e60f0bfedfb2298e2c
eunji <kej980519@gmail.com>
2023-08-24 18:44:45 +0900

    page move

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Link, Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Page1 from './Page1';
 
 function App() {
   return (
-    <></>
+    <>
+      <div>
+        <a href="/">
+          Home
+        </a>
+        <a href="Page1">
+          Page1
+        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/page1" element={<Page1 />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
