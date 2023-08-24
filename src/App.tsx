cb610c67064ded3ee68231c5ead581bde22ab3de
eunji <kej980519@gmail.com>
2023-08-24 19:48:15 +0900

    window back

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Link, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Page1 from './Page1';
+import { useEffect } from 'react';
 
 function App() {
+  window.onpageshow = function (event) {
+    if (event.persisted || (window.performance && window.performance.navigation.type == 2)) {
+      // Back Forward Cache로 브라우저가 로딩될 경우 혹은 브라우저 뒤로가기 했을 경우
+      // 이벤트 추가하는 곳
+      console.log('back button event');
+    }
+  }
   return (
     <>
       <div>
