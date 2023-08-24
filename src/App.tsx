e501c22af3a6e6a74f9ebc968aece261a2ea46a1
eunji <kej980519@gmail.com>
2023-08-24 20:59:58 +0900

    layout -fin-

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,35 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Page1 from './Page1';
-import { useEffect } from 'react';
 
 function App() {
   window.onpageshow = function (event) {
     if (event.persisted || (window.performance && window.performance.navigation.type == 2)) {
-      // Back Forward Cache·Î ºê¶ó¿ìÀú°¡ ·ÎµùµÉ °æ¿ì È¤Àº ºê¶ó¿ìÀú µÚ·Î°¡±â ÇßÀ» °æ¿ì
-      // ÀÌº¥Æ® Ãß°¡ÇÏ´Â °÷
+      // Back Forward Cacheë¡œ ë¸Œë¼ìš°ì €ê°€ ë¡œë”©ë  ê²½ìš° í˜¹ì€ ë¸Œë¼ìš°ì € ë’¤ë¡œê°€ê¸° í–ˆì„ ê²½ìš°
+      // ì´ë²¤íŠ¸ ì¶”ê°€í•˜ëŠ” ê³³
       console.log('back button event');
     }
   }
   return (
     <>
-      <div>
-        <a href="/">
+      {/* <a href="/">
           Home
         </a>
         <a href="Page1">
           Page1
-        </a>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/page1" element={<Page1 />} />
-          </Routes>
-        </BrowserRouter>
+        </a> */}
+      <div className='header'>
       </div>
+      <div className='menu'>
+        <div className='main'>
+        </div>
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
