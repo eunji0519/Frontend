66f059df2a412c7735895903a840a5ab35b79a9e
eunji <kej980519@gmail.com>
2023-09-08 00:59:35 +0900

    com

--- src/Layout/Sidebar.tsx
+++ src/Layout/Sidebar.tsx
@@ -5,7 +5,22 @@ export default function Sidebar() {
 
    const [isCheck, setCheck] = useState(false);
    const [isOpen, setOpen] = useState(false);
+   const colors = ["red", "blue", "green", "yellow", "skyblue"];
+   const colorList = colors.map((color) => <li>{color}</li>);
+   const menuLst = ["menu1", "menu2", "menu3", "menu4", "menu5"];
 
+   const [hide, setHide] = useState({
+      menu1: false,
+      menu2: false,
+      menu3: false,
+      menu4: false,
+      menu5: false,
+   });
+   const mouseEvent = (menuName: any, bool: boolean) => {
+      const change = { ...hide };
+      change[menuName] = bool;
+      setHide(change);
+   };
    return (
       <>
          <div id="sideWapper">
@@ -92,6 +107,37 @@ export default function Sidebar() {
                      )}
                   </li>
                </ul>
+            </div>
+            <div>
+               <ul>{colorList}</ul>
+            </div>
+            <div>
+               <nav className="nav">
+                  <ul className="navContainer">
+                     {menuLst.map((v, idx) => (
+                        <li
+                           className={hide[v] ? "active" : "none"}
+                           onMouseEnter={() => mouseEvent(v, true)}
+                           onMouseLeave={() => mouseEvent(v, false)}
+                        >
+                           <p>{`메뉴${idx + 1}`}</p>
+                        </li>
+                     ))}
+                  </ul>
+                  <div className="detailMenu">
+                     {menuLst.map((v, idx) => (
+                        <ul
+                           onMouseEnter={() => mouseEvent(v, true)}
+                           onMouseLeave={() => mouseEvent(v, false)}
+                        >
+                           <li>{`메뉴${idx + 1}-${1}`}</li>
+                           <li>{`메뉴${idx + 1}-${2}`}</li>
+                           <li>{`메뉴${idx + 1}-${3}`}</li>
+                           <li>{`메뉴${idx + 1}-${4}`}</li>
+                        </ul>
+                     ))}
+                  </div>
+               </nav>
             </div>
          </div>
       </>
